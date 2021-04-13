2c55158e6cd8034e8bfbe4d74bbd17a8d0b9490d
Shaggy <67756789+BobYurUncle@users.noreply.github.com>
2021-04-12 20:01:53 -0700

    8:01 updated one
    update update update update update update version

--- SystemLinearEquationsInThreeVariables.java
+++ SystemLinearEquationsInThreeVariables.java
@@ -1,15 +1,58 @@
 import java.util.Scanner;
 import java.util.Arrays;
 
+/*
+equations are represented as int[] of length 4
+
+expressions are represented as int[] of length 4
+
+systems of equations are represented as int[][]
+ */
+
 public class SystemLinearEquationsInThreeVariables {
     int[][] systemOfEquations;
 
-    //gets user's input and accordingly updates systemOfEquations
+    //gets an expression and plugs it into the system
+    private void PlugInValue(int[] inputting, int equationNum, String variable) {
+        //converting input
+        int varIndex;
+        switch (variable) {
+            case "x":
+                varIndex = 0;
+                break;
+            case "y":
+                varIndex = 1;
+                break;
+            case "z":
+                varIndex = 2;
+                break;
+            default:
+                varIndex = 100;
+        }
+        //coefficient to multiply to plugged in expression
+        int coefficientMultiply = systemOfEquations[equationNum][varIndex];
+        for (int i = 0; i < inputting.length; i++) {
+            //when isolating one variable in the equation, all the values in the expression are divided by the isolated variable's coefficient
+            inputting[i] /= inputting[varIndex];
+            //when isolating, the constant is moved to one side through subtraction and a variable is isolated to one side also through subtraction
+            //because of this, only all the variables need to be divided -1, while the constant keeps its original sign
+            if (i != 4){
+                inputting[i] *= -coefficientMultiply;
+            }
+            //the equation is changed by the plugged in expression
+            systemOfEquations[equationNum][i] += inputting[i];
+        }
+        //plugging in an expression also removes the appearance of the variable the expression was plugged into
+        systemOfEquations[equationNum][varIndex] = 0;
+    }
+
+    // gets user's input
+    // then accordingly updates systemOfEquations
     private void UserInput() {
         Scanner userInput = new Scanner(System.in);
         System.out.println("\n ----New Problem----\n");
         int amountOfEquations = 0;
-        //asking for the number of equations
+        // asking for the number of equations
         while (amountOfEquations <= 0) {
             System.out.print("How many linear equations are in your system of equations? ");
             amountOfEquations = userInput.nextInt();
@@ -19,15 +62,14 @@ public class SystemLinearEquationsInThreeVariables {
         }
         systemOfEquations = new int[amountOfEquations][4];
         System.out.println("There will be " + amountOfEquations + " equations in your system of linear equations.\n");
-        //ask for the number of variables UPDATED
-        for (int i = 0; i < amountOfEquations; i++){
+        // ask for the number of variables UPDATED
+        for (int i = 0; i < amountOfEquations; i++) {
             System.out.println("Equation " + (i + 1));
             System.out.println("Now input 4 numbers. These will be the coefficients for this equation: ()x + ()y + ()z = () ");
-            for (int x = 0; x < 4; x++){
+            for (int x = 0; x < 4; x++) {
                 systemOfEquations[i][x] = userInput.nextInt();
             }
         }
-
         /*
         ask for the number of variables
         for (int i = 0; i < amountOfEquations; i++) {
@@ -61,7 +103,7 @@ public class SystemLinearEquationsInThreeVariables {
         */
     }
 
-    //driver function
+    // driver function
     private void Solve() {
         UserInput();
         System.out.println(Arrays.toString(systemOfEquations[0]) + " " + Arrays.toString(systemOfEquations[1]));
@@ -72,3 +114,4 @@ public class SystemLinearEquationsInThreeVariables {
         problem1.Solve();
     }
 }
+
